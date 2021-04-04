c00f1cd3bd8a14e7d65a114eb12f469b4f4fcefc
wilfft <68745408+wilfft@users.noreply.github.com>
2021-04-04 20:10:39 -0300

    treino

--- src/StreamsandLambdaJavaSE11DeveloperCertification/firstLook/ifAbsentProdutoPorCategoria.java
+++ src/StreamsandLambdaJavaSE11DeveloperCertification/firstLook/ifAbsentProdutoPorCategoria.java
@@ -3,19 +3,22 @@ package StreamsandLambdaJavaSE11DeveloperCertification.firstLook;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ifAbsentProdutoPorCategoria {
     public static void main(String[] args) {
         List<Produto> produtos = DataExemplo.getProdutos();
-        HashMap<Categoria, List<Produto>> produtosPorCategoria = new HashMap<>();
+        Map<Categoria, List<Produto>> produtosPorCategoria = new HashMap<>();
 
         for (Produto produto : produtos) {
-            Categoria categoria = produto.getCategoria();
-
-            produtosPorCategoria.computeIfAbsent(categoria, c -> new ArrayList<>()).add(produto);
+            //Categoria categoria = produto.getCategoria();
+            produtosPorCategoria.computeIfAbsent(produto.getCategoria(), c -> new ArrayList<>()).add(produto);
         }
 
+        produtosPorCategoria.forEach((categoria, itens) -> {
+            System.out.println(categoria);
+            itens.forEach(e -> System.out.println("- " + e.getNome()));
+        });
     }
-
 
 }
