c1c979a334205296cae390b72e245e14127909dc
wilfft <68745408+wilfft@users.noreply.github.com>
2021-04-04 23:56:40 -0300

    Compose e andthen

---
+++ src/StreamsandLambdaJavaSE11DeveloperCertification/firstLook/TreinaComposeEAndThen.java
@@ -0,0 +1,53 @@
+package StreamsandLambdaJavaSE11DeveloperCertification.firstLook;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class TreinaComposeEAndThen {
+
+    static Optional<Produto> achaProdutoInformado(List<Produto> produtos, Predicate predicate) {
+        for (Produto produto : produtos) {
+            if (predicate.test(produto)) {
+                return Optional.of(produto);
+            }
+
+        }
+        return Optional.empty();
+    }
+
+
+    public static void main(String[] args) {
+        List<Produto> produtos = DataExemplo.getProdutos();
+        String produtoInformado = "feijao";
+        BigDecimal valorLimite = BigDecimal.valueOf(2.00);
+
+        Predicate<Produto> contemNome = e -> e.getNome().equals(produtoInformado);
+
+        Optional<Produto> produtoEncontrado = achaProdutoInformado(produtos, contemNome);
+
+        Function<Produto, BigDecimal> retornaPrecoEmValor = Produto::getPreco;
+        Function<BigDecimal, String> retornaPrecoEmTexto = e -> String.format("Produto %s encontrado, valor R$ %.2f", produtoInformado, e);
+
+        //  Function<Produto, String> combinaFunction = retornaPrecoEmValor.andThen(retornaPrecoEmTexto);
+        Function<Produto, String> combinaFunction = retornaPrecoEmTexto.compose(retornaPrecoEmValor);
+        // o compose inverte a ordem
+
+
+        produtoEncontrado.ifPresent(System.out::println);
+        achaProdutoInformado(produtos, contemNome)
+                .map(e -> e.getPreco())
+                .map(e -> String.format("Preço do %s é R$ %.2f", produtoInformado, e))
+                .ifPresentOrElse(
+                        System.out::println,
+                        () -> System.out.println("Produto não encontrado")
+                );
+        achaProdutoInformado(produtos, contemNome).map(combinaFunction)
+                .ifPresentOrElse(System.out::println,
+                        () -> System.out.println("Produto nao ecnontrado"));
+    }
+
+
+}
--- src/StreamsandLambdaJavaSE11DeveloperCertification/firstLook/TreinoProdutosPorCategoriaConsentIfAbsent.java
+++ src/StreamsandLambdaJavaSE11DeveloperCertification/firstLook/TreinoProdutosPorCategoriaConsentIfAbsent.java
@@ -1,4 +1,25 @@
 package StreamsandLambdaJavaSE11DeveloperCertification.firstLook;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class TreinoProdutosPorCategoriaConsentIfAbsent {
+
+    public static void main(String[] args) {
+
+        List<Produto> produtoList = DataExemplo.getProdutos();
+        Map<Categoria, List<Produto>> produtosPorCategoria = new HashMap<>();
+
+        for (Produto produto : produtoList) {
+            Categoria cat = produto.getCategoria();
+            produtosPorCategoria.computeIfAbsent(cat, e -> new ArrayList<>()).add(produto);
+        }
+        produtosPorCategoria.forEach((cat, itens) -> {
+                    System.out.printf("%n%s%n",cat);
+                    itens.forEach(e -> System.out.println(e.getNome()));
+                }
+        );
+    }
 }
