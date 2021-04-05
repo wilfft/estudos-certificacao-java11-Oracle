50c0d3a84ee485fc3122efa2f0b6671da3a2e4cb
wilfft <68745408+wilfft@users.noreply.github.com>
2021-04-05 11:51:38 -0300

    searching in streams

--- src/StreamsandLambdaJavaSE11DeveloperCertification/firstLook/CriandoStreams.java
+++ src/StreamsandLambdaJavaSE11DeveloperCertification/firstLook/CriandoStreams.java
@@ -1,4 +1,27 @@
 package StreamsandLambdaJavaSE11DeveloperCertification.firstLook;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class CriandoStreams {
-}
+    public static void main(String[] args) {
+
+        String[] array = new String[]{"wil", "foo"};
+        Stream<String> streamDeArray = Arrays.stream(array);
+        Stream<String> streamA = Stream.of("x", "x", "ds");
+        Stream<String> streamNulable = Stream.ofNullable("TESTE");
+        Stream<?> sreamEmpty = Stream.empty();
+
+        IntStream inteiros = new Random().ints(1, 7);
+        try {
+            BufferedReader leitor = new BufferedReader(new FileReader("listaProdutos.txt"));
+            leitor.lines().forEach(System.out::println);
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }}
---
+++ src/StreamsandLambdaJavaSE11DeveloperCertification/firstLook/FiltrandoTransformandoStreams.java
@@ -0,0 +1,40 @@
+package StreamsandLambdaJavaSE11DeveloperCertification.firstLook;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class FiltrandoTransformandoStreams {
+
+    public static void main(String[] args) {
+
+        List<Produto> lista = DataExemplo.getProdutos();
+
+        lista
+                .stream()
+                .filter(s -> s.getCategoria().equals(Categoria.COMIDA))
+                .map(Produto::getNome)
+                .forEach(System.out::println);
+
+        Pattern pattern = Pattern.compile("\\s+");
+
+        lista
+                .stream()
+                .filter(p -> p.getPreco().compareTo(BigDecimal.valueOf(2)) > 0)
+                .sorted((a, b) ->
+                        a.getNome().compareToIgnoreCase(b.getNome())
+                )
+                .forEach(System.out::println);
+
+
+        lista.stream()
+                .flatMap(e -> pattern.splitAsStream(e.getNome()))
+                .forEach(System.out::println)
+
+
+        ;
+
+    }
+
+
+}
